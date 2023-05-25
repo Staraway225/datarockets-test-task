87d229e592d834b7869ba18d0eaa66fbfa114195
Aleksei Gostev <starawayy@protonmail.com>
2023-05-25 07:49:13 +0600

    Add `create` action in `PostController`

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,21 @@
+# frozen_string_literal: true
+
 class PostController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def create
+    post = Post.new(post_params)
+    if post.valid?
+      post.save
+      render json: post
+    else
+      render json: { errors: post.errors.full_messages }
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:author, :title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :post, only: %w[create]
 end
