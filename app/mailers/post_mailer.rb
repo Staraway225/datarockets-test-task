456416ac3599cdf9c252290581d5e8da22e1e431
Aleksei Gostev <starawayy@protonmail.com>
2023-05-25 07:49:13 +0600

    Add `created_mail` mailer action

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class PostMailer < ApplicationMailer
+  def created_mail
+    @post = params[:post]
+    mail(to: @post.author, subject: 'Post was created successfully')
+  end
 end
--- spec/mailers/previews/post_mailer_preview.rb
+++ spec/mailers/previews/post_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/post_mailer
 class PostMailerPreview < ActionMailer::Preview
-
+  def created_mail
+    PostMailer.with(post: Post.last).created_mail
+  end
 end
