86b9d172f81c4a7999ccb84cbf224e54783ac117
Aleksei Gostev <starawayy@protonmail.com>
2023-05-25 07:49:11 +0600

    Write migrations

--- db/migrate/20230524212132_create_histories.rb
+++ db/migrate/20230524212132_create_histories.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 class CreateHistories < ActiveRecord::Migration[7.0]
   def change
     create_table :histories do |t|
-
-      t.timestamps
+      t.string :type, null: false
+      t.string :user, null: false
+      t.datetime :created_at, null: false, default: 'CURRENT_TIMESTAMP'
     end
   end
 end
